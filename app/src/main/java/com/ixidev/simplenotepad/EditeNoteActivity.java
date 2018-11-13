a63e6b8b48f90ef31f0b29c7fe5a2de1609c3592
pi0sik <piosikandroid96@gmail.com>
2018-11-13 23:54:40 +0100

    init

---
+++ app/src/main/java/com/ixidev/simplenotepad/EditeNoteActivity.java
@@ -0,0 +1,68 @@
+package com.ixidev.simplenotepad;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.ixidev.simplenotepad.db.NotesDB;
+import com.ixidev.simplenotepad.db.NotesDao;
+import com.ixidev.simplenotepad.model.Note;
+
+import java.util.Date;
+
+public class EditeNoteActivity extends AppCompatActivity {
+    private EditText inputNote;
+    private NotesDao dao;
+    private Note temp;
+    public static final String NOTE_EXTRA_Key = "note_id";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edite_note);
+        inputNote = findViewById(R.id.input_note);
+        dao = NotesDB.getInstance(this).notesDao();
+        if (getIntent().getExtras() != null) {
+            int id = getIntent().getExtras().getInt(NOTE_EXTRA_Key, 0);
+            temp = dao.getNoteById(id);
+            inputNote.setText(temp.getNoteText());
+        } else inputNote.setFocusable(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edite_note_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.save_note)
+            onSaveNote();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onSaveNote() {
+
+        String text = inputNote.getText().toString();
+        if (!text.isEmpty()) {
+            long date = new Date().getTime(); // get  system time
+            // if  exist update els crete new
+            if (temp == null) {
+                temp = new Note(text, date);
+                dao.insertNote(temp); // create new note and inserted to database
+            } else {
+                temp.setNoteText(text);
+                temp.setNoteDate(date);
+                dao.updateNote(temp); // change text and date and update note on database
+            }
+
+            finish(); // return to the MainActivity
+        }
+
+    }
+}
---
+++ app/src/main/java/com/ixidev/simplenotepad/callbacks/NoteEventListener.java
@@ -0,0 +1,20 @@
+package com.ixidev.simplenotepad.callbacks;
+
+import com.ixidev.simplenotepad.model.Note;
+
+
+public interface NoteEventListener {
+    /**
+     * call wen note clicked.
+     *
+     * @param note: note item
+     */
+    void onNoteClick(Note note);
+
+    /**
+     * call wen long Click to note.
+     *
+     * @param note : item
+     */
+    void onNoteLongClick(Note note);
+}
